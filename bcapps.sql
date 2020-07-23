e80518f0c7afdcffadbd7e9c08d64a294e60f912
Alvian Akmal <alvianakmaln@gmail.com>
2020-07-23 20:39:59 +0700

    baru

--- bcapps.sql
+++ bcapps.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 11 Jul 2020 pada 06.52
+-- Waktu pembuatan: 23 Jul 2020 pada 15.38
 -- Versi server: 10.1.38-MariaDB
 -- Versi PHP: 7.3.4
 
@@ -90,7 +90,9 @@ INSERT INTO `surat_masuk` (`id`, `jenis_surat`, `nomor`, `tgl`, `hal`, `asal`, `
 (4, 'Aktivasi Modul', '1209890AB', '2020-07-01', 'Impor', 'Mataram', '12:04:00', 'Bagas', '17515021', 'Diproses', ''),
 (5, 'Otoritas Pegawai', '1209890', '2020-07-01', 'Pengajuan', 'Surabaya', '09:10:00', 'Bagas', '175150218113024', 'Diterima', ''),
 (6, 'Redis PIB', '11/AM/MTR', '2020-07-02', 'Penundaan', 'Surabaya', '13:05:00', 'Bagas', '175150218113024', 'Diterima', ''),
-(7, 'Rollback CN/PIBK', '12/AP/135', '2020-06-29', 'Impor', 'Mataram', '17:18:00', 'Alvian', '1751502133', 'Diproses', '');
+(7, 'Rollback CN/PIBK', '12/AP/135', '2020-06-29', 'Impor', 'Mataram', '17:18:00', 'Alvian', '1751502133', 'Diproses', ''),
+(11, 'Redis PIB', '12', '2020-07-14', 'qna', 'sby', '16:07:00', 'a', '1324', 'diterima', ''),
+(12, 'Pembatalan CN/PIBK', '12', '2020-07-14', 'qna', 'sby', '16:10:00', 'a', '1324', 'diterima', '');
 
 -- --------------------------------------------------------
 
@@ -114,8 +116,97 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `nip`, `image`, `password`, `date_created`, `is_active`, `role_id`) VALUES
-(3, 'alvian akmal n', '175150218113024', 'me.jpg', '$2y$10$KtZicGj2HBW/VUMuOwpTD.QRJtMaDCL23BLxltqdybHYm3rr60Sr2', 0, 1, 2),
-(4, 'Amoeba', '175150218113028', 'default.jpg', '$2y$10$hS.dp8DDEeQlQc2GvdOExeHBI6znxef5lhkjJhBHNjMs5JIY7MyUS', 0, 1, 2);
+(3, 'alvian akmal n', '175150218113024', 'me.jpg', '$2y$10$jVF8sbF7i1gXcH/pFdIHo.ia4w2mnrAMWUEjyEfWSA76bUV99e0RO', 0, 1, 1),
+(4, 'Amoebaaa', '175150218113028', 'IMG_20190602_103814.jpg', '$2y$10$hS.dp8DDEeQlQc2GvdOExeHBI6znxef5lhkjJhBHNjMs5JIY7MyUS', 0, 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user_access_menu`
+--
+
+CREATE TABLE `user_access_menu` (
+  `id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `user_access_menu`
+--
+
+INSERT INTO `user_access_menu` (`id`, `role_id`, `menu_id`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 2, 2),
+(4, 1, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user_menu`
+--
+
+CREATE TABLE `user_menu` (
+  `id` int(11) NOT NULL,
+  `menu` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `user_menu`
+--
+
+INSERT INTO `user_menu` (`id`, `menu`) VALUES
+(1, 'Admin'),
+(2, 'User'),
+(3, 'Menu');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user_role`
+--
+
+CREATE TABLE `user_role` (
+  `id` int(11) NOT NULL,
+  `role` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `user_role`
+--
+
+INSERT INTO `user_role` (`id`, `role`) VALUES
+(1, 'Administrator'),
+(2, 'Member'),
+(3, 'Menu');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user_sub_menu`
+--
+
+CREATE TABLE `user_sub_menu` (
+  `id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL,
+  `title` varchar(128) NOT NULL,
+  `url` varchar(128) NOT NULL,
+  `icon` varchar(128) NOT NULL,
+  `is_active` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `user_sub_menu`
+--
+
+INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active`) VALUES
+(1, 3, 'Menu Management', 'menu', 'fas fa-fw fa-folder', 1),
+(2, 3, 'Submenu Management', 'menu/submenu', 'fas fa-fa-fw fa-folder-open', 1),
+(3, 1, 'Surat Masuk', 'admin', 'fas fa-fw fa-envelope', 1),
+(4, 2, 'My Profile', 'user', 'fas fa-fw fa-user', 1),
+(5, 1, 'Surat Keluar', 'admin/surat_k', 'fas fa-fw fa-envelope-open', 1),
+(6, 1, 'Arsip Surat', 'admin/arsip', 'fas fa-fw fa-archive', 1);
 
 --
 -- Indexes for dumped tables
@@ -146,6 +237,30 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeks untuk tabel `user_access_menu`
+--
+ALTER TABLE `user_access_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `user_menu`
+--
+ALTER TABLE `user_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `user_role`
+--
+ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `user_sub_menu`
+--
+ALTER TABLE `user_sub_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
@@ -165,13 +280,37 @@ ALTER TABLE `surat_keluar`
 -- AUTO_INCREMENT untuk tabel `surat_masuk`
 --
 ALTER TABLE `surat_masuk`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `user_access_menu`
+--
+ALTER TABLE `user_access_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `user_menu`
+--
+ALTER TABLE `user_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `user_role`
+--
+ALTER TABLE `user_role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `user_sub_menu`
+--
+ALTER TABLE `user_sub_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
